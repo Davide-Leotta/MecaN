126974244896342ed11efb4b2504df0dee6bd19e
Davide Leotta <davide.leotta05@gmail.com>
2026-08-24 16:19:47 +0200

    Fix main.gd

--- sistemi-robotici-progetto/main.gd
+++ sistemi-robotici-progetto/main.gd
@@ -6,12 +6,11 @@ extends Node3D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var posX: float = robot.position.x
 	var posZ: float = robot.position.z
-	dds.publish("posX", DDS.DDS_TYPE_FLOAT, posX)
-	dds.publish("posZ", DDS.DDS_TYPE_FLOAT, posZ)
+	dds.publish("posX", dds.DDS_TYPE_FLOAT, posX)
+	dds.publish("posZ", dds.DDS_TYPE_FLOAT, posZ)
 		
 	pass
