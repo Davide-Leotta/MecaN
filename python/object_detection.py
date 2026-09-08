041d5302d3b9ecb6be0c94939b12d8cd58c6b355
Davide Leotta <davide.leotta05@gmail.com>
2026-09-08 16:32:19 +0200

    Change logic to meter instead of pixels

--- python/object_detection.py
+++ python/object_detection.py
@@ -12,19 +12,21 @@ dds.subscribe(["colliding", "obstacle_pos_z", "obstacle_pos_x", "posZ", "posX"])
 
 width, height = 1000, 1000
 imm = np.zeros((height, width, 3), dtype=np.uint8)
-pot_field = np.zeros((height, width, 3), dtype=np.uint8)
+#pot_field = np.zeros((height, width, 3), dtype=np.uint8)
 
-scale = 5
-center_z = height // 2
-center_x = width // 2
+map_scale = 5
+center_pixel_z = height // 2
+center_pixel_x = width // 2
 
-TTL = 5.0
+TTL = 4.0
 
 valid_points = deque()
 
-target_x, target_z = 800, 800 
+target_pos_x, target_pos_z = 30, 30
+target_pixel_x = target_pos_x * map_scale + center_pixel_x
+target_pixel_z = target_pos_z * map_scale + center_pixel_z
 k_att = 0.1
-k_rep = 20000
+k_rep = 5
 
 while True:
     is_colliding = dds.read("colliding")
@@ -32,71 +34,75 @@ while True:
     robot_pos_x = dds.wait("posX")
 
     imm.fill(0)
-    pot_field.fill(0)
+    #pot_field.fill(0)
 
     now = time.time()
 
-    rpz = int(robot_pos_z * scale) + center_z
-    rpx = int(robot_pos_x * scale) + center_x
+    robot_pixel_z = int(robot_pos_z * map_scale) + center_pixel_z
+    robot_pixel_x = int(robot_pos_x * map_scale) + center_pixel_x
 
-    step = 50
-    for x in range(0, width, step):
-        for z in range(0, height, step):
-            F_att_x = k_att * (target_x - x)
-            F_att_z = k_att * (target_z - z)
-            F_rep_x = 0
-            F_rep_z = 0
+    rho_0 = 20
+    F_rep_x = 0
+    F_rep_z = 0
 
-            rho_0 = 500
+    #step = 50
+    #for x in range(0, width, step):
+    #    for z in range(0, height, step):
+    #        F_att_x = k_att * (target_x - x)
+    #        F_att_z = k_att * (target_z - z)
+    #        F_rep_x = 0
+    #        F_rep_z = 0
 
-            for expire, ox, oz, color in valid_points:
-                if color == (0, 0, 255):
-                    dist = math.hypot(x - ox, z - oz)
-                    if 0.1 < dist < rho_0:
-                        F_rep_x += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((x - ox)/dist))
-                        F_rep_z += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((z - oz)/dist))
+    #        for expire, ox, oz, color in valid_points:
+    #            if color == (0, 0, 255):
+    #                dist = math.hypot(x - ox, z - oz)
+    #                if 0.1 < dist < rho_0:
+    #                    F_rep_x += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((x - ox)/dist))
+    #                    F_rep_z += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((z - oz)/dist))
 
-            F_x = F_att_x + F_rep_x
-            F_z = F_att_z + F_rep_z
+    #        F_x = F_att_x + F_rep_x
+    #        F_z = F_att_z + F_rep_z
 
+    #        start_point = (x, z)
+    #        end_point = (int(x + F_x), int(z + F_z)) 
 
-            start_point = (x, z)
-            end_point = (int(x + F_x), int(z + F_z)) 
+    #        cv2.arrowedLine(pot_field, start_point, end_point, (255, 255, 255), 1, tipLength=0.1)
 
-            cv2.arrowedLine(pot_field, start_point, end_point, (255, 255, 255), 1, tipLength=0.1)
-
-    for expire, ox, oz, color in valid_points:
+    for expire, obstacle_pos_x, obstacle_pos_z, color in valid_points:
         if color == (0, 0, 255):
-            dist = math.hypot(rpx - ox, rpz - oz)
+            dist = math.hypot(robot_pos_x - obstacle_pos_x, robot_pos_z - obstacle_pos_z)
             if 0.01 < dist < rho_0:
-                F_rep_x += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((rpx - ox)/dist))
-                F_rep_z += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((rpz - oz)/dist))
+                F_rep_x += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((robot_pos_x - obstacle_pos_x)/dist))
+                F_rep_z += k_rep * (((1/dist) - (1/rho_0)) * (1/pow(dist, 2)) * ((robot_pos_z - obstacle_pos_z)/dist))
 
-    robot_F_x = (target_x - rpx) + F_rep_x
-    robot_F_z = (target_z - rpz) + F_rep_z
+    robot_F_x = k_att * (target_pos_x - robot_pos_x) + F_rep_x
+    robot_F_z = k_att * (target_pos_z - robot_pos_z) + F_rep_z
 
-    robot_end_point = (int(rpx + robot_F_x * k_att), int(rpz + robot_F_z * k_att))
-    cv2.arrowedLine(pot_field, (rpx, rpz), robot_end_point, (0, 255, 0), 3)
+    robot_target_pos = (robot_pos_x + robot_F_x, robot_pos_z + robot_F_z)
+    dds.publish("target_x", robot_target_pos[0], dds.DDS_TYPE_FLOAT)
+    dds.publish("target_z", robot_target_pos[1], dds.DDS_TYPE_FLOAT)
+    robot_target_pixel = int(robot_target_pos[0] * map_scale) + center_pixel_x, int(robot_target_pos[1] * map_scale) + center_pixel_z
+    cv2.arrowedLine(imm, (robot_pixel_x, robot_pixel_z), robot_target_pixel, (0, 255, 0), 3)
 
-    valid_points.append((now + TTL, rpx, rpz, (255, 0, 0)))
+    valid_points.append((now + TTL, robot_pos_x, robot_pos_z, (255, 0, 0)))
 
+    #append new obstacles
     if is_colliding:
         obstacle_pos_z = dds.read("obstacle_pos_z")
         obstacle_pos_x = dds.read("obstacle_pos_x")
 
-        opz = int(obstacle_pos_z * scale) + center_z
-        opx = int(obstacle_pos_x * scale) + center_x
+        valid_points.append((now + TTL, obstacle_pos_x, obstacle_pos_z, (0, 0, 255)))
 
-        valid_points.append((now + TTL, opx, opz, (0, 0, 255)))
-
+    #pop expired points
     while valid_points and valid_points[0][0] < now:
         valid_points.popleft()
 
+    #print points
     for expire, x, z, color in valid_points:
-        cv2.circle(imm, (x, z), 2, color, -1)
+        cv2.circle(imm, (int(x * map_scale) + center_pixel_x, int(z * map_scale) + center_pixel_z), 2, color, -1)
 
     cv2.imshow("SLAM", imm)
-    cv2.imshow("Potential Field", pot_field)
+    #cv2.imshow("Potential Field", pot_field)
 
     if cv2.waitKey(1) == ord('q'):
         break
