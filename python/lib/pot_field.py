6ffe98600685d370c6a32d3e703befca8b36344b
Davide Leotta <davide.leotta05@gmail.com>
2026-09-16 14:35:35 +0200

    Potential field refactoring and godot target visualization

---
+++ python/lib/pot_field.py
@@ -0,0 +1,36 @@
+import math
+
+
+class PotField:
+    def __init__(self, k_att, k_rep, rho_0, target_pos_z, target_pos_x):
+        self.k_att = k_att
+        self.k_rep = k_rep
+        self.rho_0 = rho_0
+        self.target_pos_z = target_pos_z
+        self.target_pos_x = target_pos_x
+
+    def evaluate(self, robot_pos_z, robot_pos_x, valid_points):
+        #calculate attractive forces
+        F_att_z = self.k_att * (self.target_pos_z - robot_pos_z)
+        F_att_x = self.k_att * (self.target_pos_x - robot_pos_x)
+
+        #calculate repulsive forces
+        F_rep_x = 0
+        F_rep_z = 0
+
+        for expire, obstacle_pos_x, obstacle_pos_z, color in valid_points:
+            if color == (0, 0, 255):
+                dist = math.hypot(robot_pos_x - obstacle_pos_x, robot_pos_z - obstacle_pos_z)
+                if 0 < dist < self.rho_0:
+                    F_rep_x += self.k_rep * (((1/dist) - (1/self.rho_0)) * (1/pow(dist, 3)) * (robot_pos_x - obstacle_pos_x))
+                    F_rep_z += self.k_rep * (((1/dist) - (1/self.rho_0)) * (1/pow(dist, 3)) * (robot_pos_z - obstacle_pos_z))
+
+        #potential field result
+        robot_F_z = F_att_z + F_rep_z
+        robot_F_x = F_att_x + F_rep_x
+
+        #potential field target
+        robot_target_pos_z = robot_pos_z + robot_F_z
+        robot_target_pos_x = robot_pos_x + robot_F_x
+
+        return robot_target_pos_z, robot_target_pos_x
--- sistemi-robotici-progetto/main.gd
+++ sistemi-robotici-progetto/main.gd
@@ -6,12 +6,18 @@ extends Node3D
 @onready var real_time_pos: Label = $"RealTimePos"
 @onready var real_time_vel: Label = $"RealTimeVel"
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	dds.subscribe("target_z")
+	dds.subscribe("target_x")
+	dds.subscribe("temp_target_z")
+	dds.subscribe("temp_target_x")
+	pass
 
 var obstacle_direction = 1
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+var temp_target_arrow: MeshInstance3D
+var final_target_arrow: MeshInstance3D
+
 func _process(delta: float) -> void:
 	var posX: float = robot.position.x
 	var posZ: float = robot.position.z
@@ -37,5 +43,37 @@ func _process(delta: float) -> void:
 	if obstacle.position.x < 5:
 		obstacle_direction = 1
 	obstacle.position.x += obstacle_direction * 0.01
+	
+	var temp_target_z = dds.read("temp_target_z")
+	var temp_target_x = dds.read("temp_target_x")
+	if temp_target_x != null and temp_target_z != null:
+		temp_target_arrow = spawn_or_update_arrow(temp_target_arrow, temp_target_x, temp_target_z, Color.BLUE)
+		
+	var target_z = dds.read("target_z")
+	var target_x = dds.read("target_x")
+	if target_x != null and target_z != null:
+		final_target_arrow = spawn_or_update_arrow(final_target_arrow, target_x, target_z, Color.GREEN)
 
-	pass
+func spawn_or_update_arrow(arrow_node: MeshInstance3D, target_x: float, target_z: float, arrow_color: Color) -> MeshInstance3D:
+	if arrow_node == null:
+		arrow_node = MeshInstance3D.new()
+		
+		var mesh = BoxMesh.new()
+		arrow_node.mesh = mesh
+		
+		var material = StandardMaterial3D.new()
+		material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		arrow_color.a = 0.5 
+		
+		material.albedo_color = arrow_color 
+		arrow_node.material_override = material
+		
+		arrow_node.rotation.x = deg_to_rad(45)
+		arrow_node.rotation.y = deg_to_rad(45)
+		arrow_node.scale = Vector3(0.25, 0.25, 0.25)
+		
+		add_child(arrow_node)
+	
+	arrow_node.position = Vector3(target_x, 1.0, target_z)
+	
+	return arrow_node
