9d2468cd1dcab82d598a497f2052d853d05ff004
Davide Leotta <davide.leotta05@gmail.com>
2026-09-23 18:33:21 +0200

    Move low pass to potential field class

--- python/lib/pot_field.py
+++ python/lib/pot_field.py
@@ -1,5 +1,6 @@
 from typing import Deque, Tuple
 from numpy.typing import NDArray
+from lib.system import *
 import math
 
 
@@ -12,6 +13,8 @@ class PotField:
         self.target_pos_x = target_pos_x
         self.F_rep_z = 0
         self.F_rep_x = 0
+        self.tmp_target_filter_z = LowPassFilter(0.1)
+        self.tmp_target_filter_x = LowPassFilter(0.1)
 
     def evaluate(self, robot_pos_z: float, robot_pos_x: float, valid_points: Deque[Tuple[int, float, float, Tuple[int, int, int]]]) -> Tuple[float, float]:
         #calculate attractive forces
@@ -36,6 +39,9 @@ class PotField:
         #potential field target
         robot_target_pos_z = robot_pos_z + robot_F_z
         robot_target_pos_x = robot_pos_x + robot_F_x
+
+        robot_target_pos_z = self.tmp_target_filter_z.evaluate(robot_target_pos_z)
+        robot_target_pos_x = self.tmp_target_filter_x.evaluate(robot_target_pos_x)
 
         return robot_target_pos_z, robot_target_pos_x
 
